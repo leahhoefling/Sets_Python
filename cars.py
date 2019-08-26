a60fda9d4da910eb6216f64457fc8d624ae9f65f
leahgwin <leahgwin@gmail.com>
2019-08-26 18:46:06 -0500

    completed next junkyard section of practice

--- cars.py
+++ cars.py
@@ -5,7 +5,7 @@ showroom = set()
 showroom = {"CRV", "Element", "Prius", "HRV"}
 
 # Print the length of your set.
-print(len(showroom))
+# print(len(showroom))
 
 # Pick one of the items in your show room and add it to the set again.
 showroom.add("HRV")
@@ -15,8 +15,29 @@ print(showroom)
 
 # Using update(), add two more car models to your showroom with another set.
 showroom.update({"Insight", "Rover"})
-print(showroom)
+# print(showroom)
 
 # You've sold one of your cars. Remove it from the set with the discard() method.
 showroom.remove("Rover")
-print(showroom)
+# print(showroom)
+
+
+# Acquiring more cars
+
+# Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
+junkyard = set()
+junkyard = {"Insight", "Yukon", "Oldsmobile", "CRV"}
+
+# Use the intersection method to see which cars exist in both the showroom and that junkyard.
+junkyard_cars_in_showroom = showroom.intersection(junkyard)
+print(junkyard_cars_in_showroom)
+
+# Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
+union_showroom = showroom.union(junkyard)
+print(union_showroom)
+
+# Use the discard() method to remove any cars that you acquired from the junkyard that you don't want in your showroom.
+union_showroom.discard("Oldsmobile")
+union_showroom.discard("Insight")
+
+print(union_showroom)
